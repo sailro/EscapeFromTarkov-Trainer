e59d30caad6623c4253ca9b4f3d00dfd499a2534
Sebastien Lebreton <sebastien@lebreton.fr>
2021-03-20 13:12:30 -0700

    Better way to outline hostiles

--- Extensions/PlayerExtensions.cs
+++ Extensions/PlayerExtensions.cs
@@ -4,11 +4,13 @@
 	{
 		public static bool IsValid(this Player player)
 		{
-			return player != null 
-			       && player.Transform != null 
-			       && player.Transform.Original != null 
-			       && player.PlayerBones != null 
-			       && player.PlayerBones.transform != null;
+			return player != null
+			       && player.Transform != null
+			       && player.Transform.Original != null
+			       && player.PlayerBones != null
+			       && player.PlayerBones.transform != null
+			       && player.PlayerBody != null
+			       && player.PlayerBody.BodySkins != null;
 		}
 
 		public static bool IsAlive(this Player player)
--- Features/Players.cs
+++ Features/Players.cs
@@ -44,6 +44,7 @@ namespace EFT.Trainer.Features
 
 		private static Color GetPlayerColor(Player player)
 		{
+			// we can use null propagation here because Profile/Info do not derive from UnityObject
 			var settings = player.Profile?.Info?.Settings;
 			if (settings != null && settings.IsBoss())
 				return BossColor;
@@ -53,87 +54,27 @@ namespace EFT.Trainer.Features
 
 		private static void SetShaders(Player player, Shader shader, Color color)
 		{
-			var transform = player.Transform.Original;
-
-			var renderers = new List<Renderer>();
-			var eligibleRenderers = new List<Renderer>();
-
-			foreach (var renderer in transform.GetComponentsInChildren<Renderer>())
+			var skins = player.PlayerBody.BodySkins;
+			foreach (var skin in skins.Values)
 			{
-				var material = renderer.material;
-				var current = material.shader;
-				if (current.name == shader.name || !current.name.StartsWith("p0"))
+				if (skin == null)
 					continue;
 
-				renderers.Add(renderer);
-			}
-
-			Dispatch(renderers, out var chestRigs, out var armors, out var tops, out var helmets, out var heads, out var others);
-
-			if (chestRigs.Any())
-				eligibleRenderers.AddRange(chestRigs);
-			else if (armors.Any())
-				eligibleRenderers.AddRange(armors);
-			else
-				eligibleRenderers.AddRange(tops);
-
-			eligibleRenderers.AddRange(helmets.Any() ? helmets : heads);
-			eligibleRenderers.AddRange(others);
-
-			foreach (var renderer in eligibleRenderers)
-			{
-				var material = renderer.material;
-				material.shader = shader;
-				material.SetColor("_FirstOutlineColor", Color.red);
-				material.SetFloat("_FirstOutlineWidth", 0.02f);
-				material.SetColor("_SecondOutlineColor", color);
-				material.SetFloat("_SecondOutlineWidth", 0.0025f);
-			}
-		}
-
-		private static void Dispatch(List<Renderer> renderers, out List<Renderer> chestRigs, out List<Renderer> armors, out List<Renderer> tops, out List<Renderer> helmets, out List<Renderer> heads, out List<Renderer> others)
-		{
-			chestRigs = new List<Renderer>();
-			armors = new List<Renderer>();
-			tops = new List<Renderer>();
-			helmets = new List<Renderer>();
-			heads = new List<Renderer>();
-			others = new List<Renderer>();
-
-			const StringComparison comparisonType = StringComparison.OrdinalIgnoreCase;
-
-			foreach (var renderer in renderers)
-			{
-				var name = renderer.name;
-				if (name.StartsWith("CR_", comparisonType))
+				foreach (var renderer in skin.GetRenderers())
 				{
-					chestRigs.Add(renderer);
-					continue;
-				}
-				if (name.StartsWith("AR_", comparisonType))
-				{
-					armors.Add(renderer);
-					continue;
-				}
-				if (name.StartsWith("Top_", comparisonType))
-				{
-					tops.Add(renderer);
-					continue;
-				}
-				if (name.StartsWith("Pants_", comparisonType))
-				{
-					others.Add(renderer);
-					continue;
-				}
-				if (name.IndexOf("_helmet_", comparisonType) >= 0)
-				{
-					helmets.Add(renderer);
-					continue;
-				}
-				if (name.IndexOf("_head_", comparisonType) >= 0)
-				{
-					heads.Add(renderer);
-					continue;
+					if (renderer == null)
+						continue;
+
+					var material = renderer.material;
+					if (material == null)
+						continue;
+
+					material.shader = shader;
+
+					material.SetColor("_FirstOutlineColor", Color.red);
+					material.SetFloat("_FirstOutlineWidth", 0.02f);
+					material.SetColor("_SecondOutlineColor", color);
+					material.SetFloat("_SecondOutlineWidth", 0.0025f);
 				}
 			}
 		}
