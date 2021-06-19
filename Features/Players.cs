89bcaf4d9ea9265783d0970a9b524d041208424f
Sebastien Lebreton <sebastien@lebreton.fr>
2021-06-19 15:21:11 -0700

    Export border color in config file

--- Features/Players.cs
+++ Features/Players.cs
@@ -12,13 +12,25 @@ namespace EFT.Trainer.Features
 		public Color BearColor { get; set; } = Color.blue;
 
 		[ConfigurationProperty]
+		public Color BearBorderColor { get; set; } = Color.red;
+
+		[ConfigurationProperty]
 		public Color UsecColor { get; set; } = Color.green;
 
 		[ConfigurationProperty]
+		public Color UsecBorderColor { get; set; } = Color.red;
+
+		[ConfigurationProperty]
 		public Color ScavColor { get; set; } = Color.yellow;
+
+		[ConfigurationProperty]
+		public Color ScavBorderColor { get; set; } = Color.red;
 		
 		[ConfigurationProperty]
 		public Color BossColor { get; set; } = Color.red;
+
+		[ConfigurationProperty]
+		public Color BossBorderColor { get; set; } = Color.red;
 
 		protected override void UpdateWhenEnabled()
 		{
@@ -31,31 +43,48 @@ namespace EFT.Trainer.Features
 				if (!ennemy.IsValid())
 					continue;
 
-				var color = GetPlayerColor(ennemy);
-				SetShaders(ennemy, GameState.OutlineShader, color);
+				GetPlayerColors(ennemy, out var color, out var borderColor);
+				SetShaders(ennemy, GameState.OutlineShader, color, borderColor);
 			}
 		}
 
-		private Color GetPlayerColor(Player player)
+		private void GetPlayerColors(Player player, out Color color, out Color borderColor)
 		{
 			var info = player.Profile?.Info;
 			if (info == null)
-				return ScavColor;
+			{
+				color = ScavColor;
+				borderColor = ScavBorderColor;
+				return;
+			}
 
 			var settings = info.Settings;
 			if (settings != null && settings.IsBoss())
-				return BossColor;
+			{
+				color = BossColor;
+				borderColor = BossBorderColor;
+				return;
+			}
 
 			// it can still be a bot in sptarkov but let's use the pmc color
-			return info.Side switch
+			switch (info.Side)
 			{
-				EPlayerSide.Bear => BearColor,
-				EPlayerSide.Usec => UsecColor,
-				_ => ScavColor
-			};
+				case EPlayerSide.Bear:
+					color = BearColor;
+					borderColor = BearBorderColor;
+					break;
+				case EPlayerSide.Usec:
+					color = UsecColor;
+					borderColor = UsecBorderColor;
+					break;
+				default:
+					color = ScavColor;
+					borderColor = ScavBorderColor;
+					break;
+			}
 		}
 
-		private static void SetShaders(Player player, Shader shader, Color color)
+		private static void SetShaders(Player player, Shader shader, Color color, Color borderColor)
 		{
 			var skins = player.PlayerBody.BodySkins;
 			foreach (var skin in skins.Values)
@@ -77,7 +106,7 @@ namespace EFT.Trainer.Features
 
 					material.shader = shader;
 
-					material.SetColor("_FirstOutlineColor", Color.red);
+					material.SetColor("_FirstOutlineColor", borderColor);
 					material.SetFloat("_FirstOutlineWidth", 0.02f);
 					material.SetColor("_SecondOutlineColor", color);
 					material.SetFloat("_SecondOutlineWidth", 0.0025f);
