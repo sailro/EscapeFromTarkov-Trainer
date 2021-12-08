2b837417ec901b6a12a6d5d188698aa278926dd5
Sebstien Lebreton <sebastien@lebreton.fr>
2021-12-08 11:01:56 +0100

    Add skills feature

---
+++ Features/Skills.cs
@@ -0,0 +1,35 @@
+﻿using EFT.Trainer.Extensions;
+using JetBrains.Annotations;
+using UnityEngine;
+
+#nullable enable
+
+namespace EFT.Trainer.Features
+{
+	[UsedImplicitly]
+	internal class Skills : TriggerFeature
+	{
+		public override string Name => "skills";
+
+		public override KeyCode Key { get; set; } = KeyCode.None;
+
+		protected override void UpdateWhenTriggered()
+		{
+			var player = GameState.Current?.LocalPlayer;
+			if (!player.IsValid())
+				return;
+
+			if (player.Skills?.Skills == null)
+				return;
+
+			foreach (var skill in player.Skills.Skills)
+				skill.SetLevel(51);
+
+			if (player.Skills.Mastering == null)
+				return;
+
+			foreach (var item in player.Skills.Mastering.Values)
+				item.Current = item.MasteringGroup.Level2 + item.MasteringGroup.Level3;
+		}
+	}
+}
