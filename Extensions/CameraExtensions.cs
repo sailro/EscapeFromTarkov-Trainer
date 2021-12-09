e6143ea89ef4f174598d27a50e218cc691e3e1ae
Sebstien Lebreton <sebastien@lebreton.fr>
2021-12-09 20:11:32 +0100

    Suppress

--- Extensions/CameraExtensions.cs
+++ Extensions/CameraExtensions.cs
@@ -13,7 +13,9 @@ namespace EFT.Trainer.Extensions
 			return screenPoint;
 		}
 
+#pragma warning disable IDE0060 // Remove unused parameter
 		public static bool IsScreenPointVisible(this Camera camera, Vector3 screenPoint)
+#pragma warning restore IDE0060 // Remove unused parameter
 		{
 			return screenPoint.z > 0.01f && screenPoint.x > -5f && screenPoint.y > -5f && screenPoint.x < Screen.width && screenPoint.y < Screen.height;
 		}
