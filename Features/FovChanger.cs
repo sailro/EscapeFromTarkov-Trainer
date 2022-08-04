8bf8a52881841b8199c80b045e8894f22db89623
Chishiki <finn@pelzers.com>
2022-08-04 18:02:23 +0200

    added toggle feature

---
+++ Features/FovChanger.cs
@@ -0,0 +1,27 @@
+﻿using EFT.Trainer.Configuration;
+using JetBrains.Annotations;
+
+namespace EFT.Trainer.Features
+{
+	[UsedImplicitly]
+	internal class FovChanger : ToggleFeature
+	{
+		public override string Name => "fovchanger";
+		[ConfigurationProperty(Order = 1)]
+		public override bool Enabled { get; set; } = true;
+
+		[ConfigurationProperty(Order = 3)]
+		public float Fov { get; set; } = 90;
+
+		protected override void LateUpdateWhenEnabled()
+		{
+			var camera = GameState.Current?.Camera;
+
+			if (camera == null)
+				return;
+
+			if (Enabled)
+				camera.fieldOfView = Fov;
+		}
+	}
+}
