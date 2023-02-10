66b465eb2d7436bef8b34242385654de7eceeb3b
Solareon <769465+solareon@users.noreply.github.com>
2023-02-10 23:42:43 +0100

    Fixed with reworked prefix
    Less hacky and more demi-god like. You can still be killed but only by super high collateral damage (sitting on top of 2 grenades).

--- Features/Health.cs
+++ Features/Health.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using EFT.Trainer.Configuration;
 using EFT.Trainer.Extensions;
 using EFT.Trainer.Model;
 using JetBrains.Annotations;
-using UnityEngine;
 
 #nullable enable
 
@@ -28,11 +25,9 @@ namespace EFT.Trainer.Features
 		public bool FoodWater { get; set; } = true;
 
 		private static readonly Array _bodyParts = Enum.GetValues(typeof(EBodyPart));
-
-		private static String[] TargetBones = { "head", "spine3", "spine2", "spine1" };
-
+		
 		[UsedImplicitly]
-		protected static bool ApplyDamagePrefix(object? __instance, ref float __result)
+		protected static bool ApplyDamagePrefix(EBodyPart bodyPart, object? __instance, ref float __result)
 		{
 			var feature = FeatureFactory.GetFeature<Health>();
 			if (feature == null || !feature.Enabled || __instance == null)
@@ -46,34 +41,15 @@ namespace EFT.Trainer.Features
 			
 			if (feature.VitalsOnly)
 			{
-				__result = 0.01f; // hack to allow damage but not enough to kill you. Should probably look at the body part about to receive damage and set to zero
-				return true;
+				if (bodyPart == EBodyPart.Chest || bodyPart == EBodyPart.Head)
+				{
+					__result = 0f;
+					return false; // skip the original code because we must protect the head and chest
+				}
+				return true; // keep using original code, apply damage to extremities 
 			}
-				__result = 0f;
+			__result = 0f;
 			return false;  // skip the original code and all other prefix methods 
-		}
-
-		private static IEnumerable<Transform> EnumerateHierarchyCore(Transform root)
-		{
-			Queue<Transform> transformQueue = new Queue<Transform>();
-			transformQueue.Enqueue(root);
-
-			while (transformQueue.Count > 0)
-			{
-				Transform parentTransform = transformQueue.Dequeue();
-
-				if (!parentTransform)
-				{
-					continue;
-				}
-
-				for (Int32 i = 0; i < parentTransform.childCount; i++)
-				{
-					transformQueue.Enqueue(parentTransform.GetChild(i));
-				}
-
-				yield return parentTransform;
-			}
 		}
 
 		protected override void UpdateWhenEnabled()
@@ -99,20 +75,8 @@ namespace EFT.Trainer.Features
 				harmony.Patch(original, new HarmonyLib.HarmonyMethod(prefix));
 			});
 
-			if (VitalsOnly)
-			{
-				foreach (Transform transform in Health.EnumerateHierarchyCore(player.gameObject.transform).Where(t => TargetBones.Any(u => t.name.ToLower().Contains(u))))
-				{
-					if (transform.gameObject.layer != LayerMask.NameToLayer("PlayerSpiritAura"))
-					{
-						transform.gameObject.layer = LayerMask.NameToLayer("PlayerSpiritAura"); // Theoretically makes it impossible to hit your head/chest. Can still die from collateral damage.
-					}
-				}
-			}
-
 			foreach (EBodyPart bodyPart in _bodyParts)
 			{
-
 				if (bodyPart == EBodyPart.Common)
 					continue;
 
@@ -124,8 +88,8 @@ namespace EFT.Trainer.Features
 
 				if (RemoveNegativeEffects)
 				{
-					healthController.RemoveNegativeEffects(EBodyPart.Common);
 					healthController.RemoveNegativeEffects(bodyPart);
+					healthController.RemoveNegativeEffects(EBodyPart.Common);
 				}
 
 				var bodyPartHealth = healthController.GetBodyPartHealth(bodyPart);
@@ -145,6 +109,7 @@ namespace EFT.Trainer.Features
 
 			if (!healthController.Hydration.AtMaximum && FoodWater)
 				healthController.ChangeHydration(healthController.Hydration.Maximum);
+
 		}
 	}
 }
