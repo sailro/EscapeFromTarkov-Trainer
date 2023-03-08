0b2932a97b12df72367461334d240fd08fd07ea7
Sebastien Lebreton <sebastien@lebreton.fr>
2023-03-08 16:40:34 +0100

    Rename

--- Features/BaseMapToggleFeature.cs
+++ Features/BaseMapToggleFeature.cs
@@ -4,23 +4,23 @@ namespace EFT.Trainer.Features
 {
 	internal abstract class BaseMapToggleFeature : ToggleFeature
 	{
-		private GameObject? _radarCameraObject = null;
-		private Camera? _radarCamera = null;
+		private GameObject? _mapCameraObject = null;
+		private Camera? _mapCamera = null;
 
 		protected void ToggleMapCameraIfNeeded(bool state)
 		{
-			if (_radarCamera == null)
+			if (_mapCamera == null)
 				return;
 
-			if (_radarCamera.enabled == state)
+			if (_mapCamera.enabled == state)
 				return;
 
-			_radarCamera.enabled = state;
+			_mapCamera.enabled = state;
 		}
 
 		protected void SetupMapCameraOnce(Camera camera, float x, float y, float sizex, float sizey)
 		{
-			if (_radarCameraObject != null)
+			if (_mapCameraObject != null)
 			{
 				ToggleMapCameraIfNeeded(true);
 				return;
@@ -29,16 +29,14 @@ namespace EFT.Trainer.Features
 			// We need to setup weather for proper rendering
 			Weather.ToClearWeather();
 
-			_radarCameraObject = new GameObject(GetType().FullName + nameof(_radarCameraObject), typeof(Camera), typeof(PrismEffects));
-			_radarCameraObject.GetComponent<PrismEffects>().CopyComponentValues(camera.GetComponent<PrismEffects>());
-			_radarCamera = _radarCameraObject.GetComponent<Camera>();
-			_radarCamera.name = GetType().FullName + nameof(_radarCamera);
-			_radarCamera.pixelRect = new Rect(x, y, sizex, sizey);
-			_radarCamera.allowHDR = false;
-			_radarCamera.depth = -1;
+			_mapCameraObject = new GameObject(GetType().FullName + nameof(_mapCameraObject), typeof(Camera), typeof(PrismEffects));
+			_mapCameraObject.GetComponent<PrismEffects>().CopyComponentValues(camera.GetComponent<PrismEffects>());
+			_mapCamera = _mapCameraObject.GetComponent<Camera>();
+			_mapCamera.name = GetType().FullName + nameof(_mapCamera);
+			_mapCamera.pixelRect = new Rect(x, y, sizex, sizey);
+			_mapCamera.allowHDR = false;
+			_mapCamera.depth = -1;
 
-			// When using several cameras
-			// _radarCamera.transparencySortMode = TransparencySortMode.CustomAxis;
 
 			// Prevent NullReferenceException in PrismEffects 
 			GameWorld.OnDispose -= UpdateWhenDisabled;
@@ -47,12 +45,12 @@ namespace EFT.Trainer.Features
 
 		protected void UpdateMapCamera(Transform cameraTransform, float range)
 		{
-			if (_radarCameraObject == null) 
+			if (_mapCameraObject == null) 
 				return;
 
-			var radarCameraTransform = _radarCameraObject.transform;
-			radarCameraTransform.eulerAngles = new Vector3(90, cameraTransform.eulerAngles.y, cameraTransform.eulerAngles.z);
-			radarCameraTransform.localPosition = new Vector3(cameraTransform.localPosition.x, range * Mathf.Tan(45), cameraTransform.localPosition.z);
+			var mapTransform = _mapCameraObject.transform;
+			mapTransform.eulerAngles = new Vector3(90, cameraTransform.eulerAngles.y, cameraTransform.eulerAngles.z);
+			mapTransform.localPosition = new Vector3(cameraTransform.localPosition.x, range * Mathf.Tan(45), cameraTransform.localPosition.z);
 		}
 	}
 }
