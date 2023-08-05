0b6d1d512383f53ee0cea1586d88bc1a0def0da1
Solareon <769465+solareon@users.noreply.github.com>
2023-08-05 13:08:10 +0200

    Bump release number

--- Installer/Properties/AssemblyInfo.cs
+++ Installer/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
 [assembly: AssemblyProduct("Installer")]
-[assembly: AssemblyCopyright("Copyright © 2022 Sebastien Lebreton")]
+[assembly: AssemblyCopyright("Copyright © 2023 Sebastien Lebreton")]
