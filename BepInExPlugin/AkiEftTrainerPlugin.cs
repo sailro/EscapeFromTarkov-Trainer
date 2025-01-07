4e2ffd7fe37cb9366d16a37f25c117ad1fb029c3
Sebastien Lebreton <sebastien@lebreton.fr>
2025-01-07 22:03:57 +0100

    Whitelist this plugin for spt-aki beta releases

--- BepInExPlugin/AkiEftTrainerPlugin.cs
+++ BepInExPlugin/AkiEftTrainerPlugin.cs
@@ -1,11 +1,15 @@
-﻿using BepInEx;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using BepInEx;
 using EFT.Trainer;
 using JetBrains.Annotations;
 
-[BepInPlugin("com.spt-aki.efttrainer", "AKI.EftTrainer", "1.0.0")]
+[BepInPlugin(PluginId, "AKI.EftTrainer", "1.0.0")]
 [UsedImplicitly]
 public class AkiDebuggingPlugin : BaseUnityPlugin
 {
+	private const string PluginId = "com.spt-aki.efttrainer";
 	public static bool Loaded = false;
 
 	[UsedImplicitly]
@@ -16,6 +20,22 @@ public class AkiDebuggingPlugin : BaseUnityPlugin
 
 		Loader.Load();
 		Loaded = true;
+
+		WhitelistThisPlugin();
 	}
 
+	private static void WhitelistThisPlugin()
+	{
+		// Whitelist this plugin for spt-aki beta releases
+		var type = Type.GetType("SPT.Custom.Utils.MenuNotificationManager, spt-custom", throwOnError: false);
+		if (type == null)
+			return;
+
+		var field = type.GetField("whitelistedPlugins", BindingFlags.NonPublic | BindingFlags.Static);
+		if (field == null)
+			return;
+
+		var hashset = field.GetValue(null) as HashSet<string>;
+		hashset?.Add(PluginId);
+	}
 }
