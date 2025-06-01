e6dfea44bb529fed8bfbf801291e265e2b6c4125
Sebastien Lebreton <sebastien@lebreton.fr>
2025-06-01 16:42:28 +0200

    Add support for SPT 4.0 (#714)
    * Rework bepinex plugin
    
    * Use event instead of polling
    
    * Remove legacy nlog entrypoint

--- BepInExPlugin/SptEftTrainerPlugin.cs
+++ BepInExPlugin/SptEftTrainerPlugin.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using BepInEx;
 using EFT.Trainer;
 using JetBrains.Annotations;
+using UnityEngine.SceneManagement;
 
 [BepInPlugin(PluginId, "SPT.EftTrainer", "1.0.0")]
 [UsedImplicitly]
@@ -15,17 +16,40 @@ public class SptEftTrainerPlugin : BaseUnityPlugin
 	[UsedImplicitly]
 	public void Awake()
 	{
+		SetupSptBetaReleases();
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+	{
+		LoadTrainer();
+
 		if (Loaded)
-			return;
-
-		Loader.Load();
-		Loaded = true;
-
-		HandleSptBetaReleases();
+			SceneManager.sceneLoaded -= OnSceneLoaded;
 	}
 
 	[UsedImplicitly]
 	public void OnGUI()
+	{
+		RemoveSptBetaReleaseWatermark();
+	}
+
+	private void LoadTrainer()
+	{
+		if (Loaded)
+			return;
+
+		var scene = SceneManager.GetActiveScene();
+		if (string.IsNullOrEmpty(scene.name))
+			return;
+
+		Logger.LogInfo($"Found {scene.name}, loading...");
+
+		Loader.Load();
+		Loaded = true;
+	}
+
+	private void RemoveSptBetaReleaseWatermark()
 	{
 		if (_commitHash == null)
 			return;
@@ -33,27 +57,37 @@ public class SptEftTrainerPlugin : BaseUnityPlugin
 		var hash = _commitHash.GetValue(null) as string;
 		if (hash == string.Empty)
 		{
+			Logger.LogInfo("Disabling SPT hash monitoring");
+
 			// Stop monitoring
 			_commitHash = null;
 			return;
 		}
 
 		// Suppress this dumb watermark
+		Logger.LogInfo("Removing SPT watermark");
 		_commitHash.SetValue(null, string.Empty);
 	}
 
 	private static FieldInfo _commitHash;
-	private static void HandleSptBetaReleases()
+	private void SetupSptBetaReleases()
 	{
 		// Whitelist this plugin for spt beta releases
 		var menuNotificationManager = Type.GetType("SPT.Custom.Utils.MenuNotificationManager, spt-custom", throwOnError: false);
 		if (menuNotificationManager == null)
+		{
+			Logger.LogInfo("MenuNotificationManager not found");
 			return;
+		}
 
 		var hashField = GetStaticField(menuNotificationManager, "whitelistedPlugins");
 		if (hashField == null)
+		{
+			Logger.LogInfo("whitelistedPlugins not found");
 			return;
+		}
 
+		Logger.LogInfo($"White-listing {PluginId}");
 		var hashset = hashField.GetValue(null) as HashSet<string>;
 		hashset?.Add(PluginId);
 
