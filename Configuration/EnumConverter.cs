a46682c0438cdc0cd5aca77b6d003bfb9ee36e6c
Sebastien Lebreton <sebastien@lebreton.fr>
2025-10-13 14:33:24 +0200

    Refactor enum converters

---
+++ Configuration/EnumConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace EFT.Trainer.Configuration;
+
+public class EnumConverter<T> : JsonConverter where T : Enum
+{
+	public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+	{
+		value ??= default(T)!;
+		serializer.Serialize(writer, Enum.GetName(typeof(T), value));
+	}
+
+	public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+	{
+		var value = serializer.Deserialize<string>(reader);
+		return value == null ? default(T)! : Enum.Parse(typeof(T), value);
+	}
+
+	public override bool CanConvert(Type objectType)
+	{
+		return objectType == typeof(T);
+	}
+}
--- Configuration/KeyCodeConverter.cs
+++ Configuration/KeyCodeConverter.cs
@@ -6,22 +6,4 @@ using UnityEngine;
 
 namespace EFT.Trainer.Configuration;
 
-public class KeyCodeConverter : JsonConverter
-{
-	public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
-	{
-		value ??= KeyCode.None;
-		serializer.Serialize(writer, Enum.GetName(typeof(KeyCode), value));
-	}
-
-	public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
-	{
-		var value = serializer.Deserialize<string>(reader);
-		return value == null ? KeyCode.None : Enum.Parse(typeof(KeyCode), value);
-	}
-
-	public override bool CanConvert(Type objectType)
-	{
-		return objectType == typeof(KeyCode);
-	}
-}
+public class KeyCodeConverter : EnumConverter<KeyCode>;
